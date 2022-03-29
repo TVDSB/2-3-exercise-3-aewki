f882182eb38e57e0e603f111e5421ad2b5518ab8
aewki <100421706+aewki@users.noreply.github.com>
2022-03-29 08:44:54 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,15 @@
 def main():
     #your code goes here
-    print("0")
+    X = input()
+    Y = int(X)
+    if Y %3 == 0 and Y %5 == 0:
+        print("fizzbuzz")
+    elif Y %3 == 0:
+        print("fizz")
+    elif Y %5 == 0:
+        print("buzz")
+    else:
+        print("Y")
 
 if __name__ =='__main__':
     main()
